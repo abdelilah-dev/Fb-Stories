2f7ae62c6c9043a8a4ec5f38849813fed22076e5
AbdeLilah-Dev <contactme.abdelilah@gmail.com>
2025-04-12 17:28:10 +0100

    Upload the Update

--- main.js
+++ main.js
@@ -69,6 +69,7 @@ const allStories = [
 const stories = document.querySelector(".stories");
 const storiesFullView = document.querySelector(".stories-full-view");
 const closeFullViewBtn = document.querySelector(".close-btn");
+const storiesContent = document.querySelector(".stories-container .content");
 const previewBtn = document.querySelector(".stories-container .previous-btn");
 const nextBtn = document.querySelector(".stories-container .next-btn");
 const previewFullViewBtn = document.querySelector(".stories-full-view .previous-btn");
@@ -76,10 +77,10 @@ const nextFullViewBtn = document.querySelector(".stories-full-view .next-btn");
 
 let currentActive = 0;
 
-// window.onload = () => {
-//     console.log("page is loaded");
-//     createStories()
-// }
+window.onload = () => {
+    console.log("page is loaded");
+    createStories()
+}
 
 const createStories = () => {
     allStories.forEach((s, i) => {
@@ -102,6 +103,31 @@ const createStories = () => {
     })
 }
 
+nextBtn.addEventListener("click", event => {
+    storiesContent.scrollLeft += 300;
+    console.log(scrollX) ;
+})
+
+previewBtn.addEventListener("click", event => {
+    storiesContent.scrollLeft -= 300;
+    console.log(scrollX) ;
+})
+
+storiesContent.addEventListener("scroll", event => {
+    if (storiesContent.scrollLeft <= 24) {
+        previewBtn.classList.remove("active");
+    } else {
+        previewBtn.classList.add("active");
+    }
+
+    let maxScrollValue = storiesContent.scrollWidth - storiesContent.clientWidth - 24;
+    if (storiesContent.scrollLeft >= maxScrollValue) {
+        nextBtn.classList.remove("active");
+    } else {
+        nextBtn.classList.add("active")
+    }
+})
+
 const showFullView = (currentStoryIndex) => {
     currentActive = currentStoryIndex;
     updateFullView();
@@ -115,20 +141,26 @@ closeFullViewBtn.addEventListener("click", event => {
 
 previewFullViewBtn.addEventListener("click", event => {
     currentActive -= currentActive > 0 ? 1 : 0;
-    console.log(currentActive)
     updateFullView();
 })
 
 nextFullViewBtn.addEventListener("click", event => {
     currentActive += currentActive < allStories.length - 1 ? 1 : 0;
-    console.log(currentActive)
     updateFullView();
 })
 
 const updateFullView = () => {
+    if (currentActive === 0) {
+        previewFullViewBtn.classList.remove("active");
+    } else if (currentActive === allStories.length - 1) {
+        nextFullViewBtn.classList.remove("active");
+    } else {
+        previewFullViewBtn.classList.add("active");
+        nextFullViewBtn.classList.add("active");
+    }
     let storyFullView = document.querySelector(".stories-full-view .story")
     storyFullView.innerHTML = `
         <img src="${allStories[currentActive].imageUrl}" alt="">
-        <div class="${allStories[currentActive].author}">Author</div>
+        <div class="author">${allStories[currentActive].author}</div>
     `
 }
